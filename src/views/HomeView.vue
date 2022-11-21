c550ecec605d36f629ab414a089b915901ad6e43
unknown <HuyenHoang@huyenht27.officehn.local>
2022-11-21 11:23:10 +0700

    edit detail

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,19 @@
   <div class="container">
     <div class="col col-12 mt-4 mb-4">
       <result-table-vue></result-table-vue>
+      <div class="item-center">
+        <ul class="list-number list-group-light color-white">
+            <li class="d-flex justify-content-center align-items-center bg-white">
+              Chế độ View
+            </li>
+            <li class="list-group-item d-flex justify-content-center align-items-center">
+              <router-link to="/" class="edit-tag-a">Home</router-link>
+            </li>
+            <li class="list-group-item d-flex justify-content-center align-items-center">
+              <router-link to="/opposite-pair" class="edit-tag-a">Detail</router-link>
+            </li>
+        </ul>
+    </div>
     </div>
   </div>
 </template>
