908699ccc221dd983a6fa3d9d86fcd4d283cd39d
khanhph4 <khanhph4@fpt.com.vn>
2022-11-21 15:33:05 +0700

    update view: add menu, update Home

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <router-view />
-  <!-- <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/detail">Detail</router-link>
-  </nav> -->
+  <div class="menu">
+    <div class="bg-white">Chế độ View</div>
+    <router-link to="/" class="row ms-2 edit-tag-a"><b>1. Tổng BXH</b></router-link>
+    <router-link to="/opposite-pair" class="row ms-2 edit-tag-a"
+      ><b>2. Cặp đấu</b></router-link
+    >
+  </div>
   <div class="wrap-footer mt-5"></div>
 </template>
 
